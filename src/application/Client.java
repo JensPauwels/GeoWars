6f8bea892a73fa55fbb1ad1a4fc52c53c7240031
JensPauwels <jens.pauwels@hotmail.com>
2016-11-14 14:06:16 +0100

    boss

--- src/application/Client.java
+++ src/application/Client.java
@@ -6,6 +6,7 @@ public class Client {
     public static void main(String[] args){
         UserInterface.launch(UserInterface.class,args);
         //halloo grietje
+        //Michiel is nen baas
 
     }
 }
